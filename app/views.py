d3d47102be58cb88f083ea32a7abd913702af0d4
theanh <theanhvo221@gmail.com>
2017-07-03 16:35:20 +0700

    update

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.utils import timezone
 
@@ -33,3 +34,14 @@ class PostListView(ListView):
                 result = result.filter(restaurant_namme__unaccent=search_list[1])
 
         return result
+
+
+class PostDetailView(DetailView):
+
+
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
