ecb46dcb9ea5db194cce29d650f0f8b508e40482
Hai Son <shnt1221@gmail.com>
2017-07-03 17:18:45 +0700

    rename template

--- app_work/urls.py
+++ app_work/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('userena.urls')),
     url(r'^',include('app.urls')),
+    url(r'^accounts/', include('userena.urls')),
 ]
