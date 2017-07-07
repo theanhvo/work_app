24b78901119dac471d9d68b6de613d462e8f8c8a
theanh <theanhvo221@gmail.com>
2017-07-07 16:43:39 +0700

    update templates

--- crawler.py
+++ crawler.py
@@ -13,14 +13,8 @@ from app import models
 
 LOGGER = logging.getLogger(__name__)
 
-
-def crawl_detail(link):
-    """
-    Request to fetch page from url & store Post & Job
-    """
+def crawl_template1(link , page):
     LOGGER.info('Crawl detail page %r', link)
-    resp = requests.get(link)
-    page = BeautifulSoup(resp.content, "lxml")
     box_content = page.find("div", {"class" : "detail-content box-content"})
     name_work = box_content.h1.text
     print(name_work)
@@ -95,13 +89,14 @@ def crawl_detail(link):
         for row in rows:
             title = row.b.text
             if title == 'Mô tả':
-                job_desc = row.p.text.strip()
-
+                job_desc1 = row.p.text.strip()
+                job_desc = job_desc1.replace('-' , '\n')
             elif title == 'Yêu cầu':
-                job_requirements = row.p.text.strip()
-
+                job_requirements1 = row.p.text.strip()
+                job_requirements = job_requirements1.replace('-' , '\n')
             elif title == 'Quyền lợi':
-                benefit = row.p.text.strip()
+                benefit1 = row.p.text.strip()
+                benefit = benefit1.replace('-' , '\n')
 
             elif title == 'Hạn nộp':
                 deadline = row.find('b', {'class': 'text-danger'}).text.strip()
@@ -118,8 +113,10 @@ def crawl_detail(link):
                 address = row.p.text.strip()
 
     if thanh_pho == 'Hồ Chí Minh':
-        thanh_pho = Post.SG
+        thanh_pho = models.Post.SG
 
+    if thanh_pho == "Hà Nội":
+        thanh_pho == models.Post.HN
 
     post_params = dict(
         logo_restaurant=link_img,
@@ -155,6 +152,138 @@ def crawl_detail(link):
         job.post.add(post)
         job.save()
 
+def crawl_template2(link, page):
+    LOGGER.info('Crawl detail page %r', link)
+    tenviec = page.find("div" , {"class" : "info-left"})
+    name_work = tenviec.find('h1').text
+    print(name_work)
+
+    link_pic = page.find("span" , {"id" : "logo_sv_lg"})
+    img = link_pic.img
+    link_img = img.get('src')
+    print(link_img)
+
+    company_name = page.find("div" , {"class" : "title-banner"})
+    restaurant_name = company_name.h2.text
+    print(restaurant_name)
+
+    dia_chi =page.find_all("div" , {"class" : "both-contact"})
+    rows = []
+    if len(dia_chi) >= 1:
+        rows = dia_chi[0].find_all('span')
+        dong  = rows[1].text.strip()
+        dong_split = dong.split(':')
+        address = dong_split[1]
+        lien_he =rows[0].text
+        lien_he1 = lien_he.split(":")
+        employer_contact = lien_he1[1].strip()
+    print(address)
+    print(employer_contact)
+    divtag = page.find_all("div" , {"class" : "col-lg-6 col-md-12 col-sm-12 col-xs-12 pl0 pr0"})
+    muc_luong1 = divtag[0].text
+    muc_luong2 = muc_luong1.split(':')
+    muc_luong_result = muc_luong2[1].strip()
+    print(muc_luong_result)
+
+    city1 = divtag[1].text
+    city2 = city1.split(':')
+    thanh_pho = city2[1].strip()
+
+    kinh_nghiem1 = divtag[2].text
+    kinh_nghiem2 = kinh_nghiem1.split(':')
+    kinh_nghiem_result = kinh_nghiem2[1].strip()
+    print(kinh_nghiem_result)
+
+    trinhdo1 = divtag[3].text
+    trinhdo2 = trinhdo1.split(':')
+    trinh_do = trinhdo2[1].strip()
+    print(trinh_do)
+
+    hinhthuc1 = divtag[4].text
+    hinhthuc2 = hinhthuc1.split(':')
+    hinh_thuc = hinhthuc2[1].strip()
+    print(hinh_thuc)
+
+    soluong1 = divtag[5].text
+    soluong2 = soluong1.split(':')
+    so_luong = soluong2[1].strip()
+    print(so_luong)
+
+    gioitinh1 = divtag[6].text
+    gioitinh2 = gioitinh1.split(':')
+    gioi_tinh = gioitinh2[1].strip()
+    print(gioi_tinh)
+
+    hannop = page.find("div" , {"class" : "hnhs"})
+    hannop1 = hannop.text
+    hannop2 = hannop1.split(':')
+    deadline = hannop2[1].strip()
+    print(deadline)
+
+    des_info = page.find("div" , {"class" : "des-info-job"})
+    des_info_result = des_info.find_all('p')
+    for lines in des_info_result:
+        job_desc = des_info_result[0].text
+        print(job_desc)
+        job_requirements = des_info_result[1].text
+        print(job_requirements)
+        benefit1 = des_info_result[2].text
+        benefit = benefit1.replace("-", "").strip()
+        print(benefit)
+
+    if thanh_pho == 'Hồ Chí Minh':
+        thanh_pho = models.Post.SG
+
+    if thanh_pho == "Hà Nội":
+        thanh_pho == models.Post.HN
+
+    post_params = dict(
+        logo_restaurant=link_img,
+        restaurant_namme=restaurant_name,
+        address=address,
+        city=thanh_pho,
+        wage=muc_luong_result,
+        contact_employer=employer_contact,
+        experience=kinh_nghiem_result,
+        number_recruits=so_luong,
+        gender=gioi_tinh,
+        type_job=hinh_thuc,
+        job_description=job_desc,
+        job_requirements=job_requirements,
+        why_love_this_job=benefit,
+        deadline=deadline,
+    )
+
+    for k in post_params:
+        LOGGER.debug('****')
+        LOGGER.debug(f'{k}: \n {post_params[k]}')
+
+    if not models.Post.objects.filter( # kiem tra ten nha hang ...neu ko co them moi
+        restaurant_namme=restaurant_name,
+        job_description=job_desc,
+        deadline=deadline,
+        ).exists():
+        LOGGER.info('Store new restaurant %r', post_params)
+
+        post = models.Post.objects.create(**post_params)
+        job, _ = models.Job.objects.get_or_create(name=name_work)
+        job.post.add(post)
+        job.save()
+
+
+def crawl_detail(link):
+    """
+    Request to fetch page from url & store Post & Job
+    """
+    LOGGER.info('Crawl detail page %r', link)
+
+    resp = requests.get(link)
+    page = BeautifulSoup(resp.content, "lxml")
+    box_content = page.find("div", {"class" : "detail-content box-content"})
+    if box_content == None:
+        crawl_template2(link, page)
+    else:
+        crawl_template1(link, page)
 
 if __name__ == '__main__':
     url = "https://www.timviecnhanh.com/viec-lam-du-lich-nha-hang-khach-san-tai-ho-chi-minh-f23p1.html?page=1"
